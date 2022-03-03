98f489cf155c380a55a14ef011febb022c4909d2
Oleksandr Martyniuk <alexander.martyniuk@gmail.com>
2022-03-02 20:39:35 -0500

    Added EntityFramework reference, DBContext and migration

---
+++ src/RussianSitesStatus/Database/Models/Entity.cs
@@ -0,0 +1,37 @@
+﻿namespace RussianSitesStatus.Database.Models;
+
+public class Entity : IEquatable<Entity>
+{
+    public long Id { get; set; }
+
+    public override int GetHashCode()
+    {
+        return (int)Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        var entity = obj as Entity;
+        if (entity == null)
+        {
+            return false;
+        }
+
+        return entity.Id == this.Id;
+    }
+
+    public override string ToString()
+    {
+        return $"{GetType().Name} [{Id}]";
+    }
+
+    public bool Equals(Entity other)
+    {
+        if (other == null)
+        {
+            return false;
+        }
+
+        return other.Id == this.Id;
+    }
+}
