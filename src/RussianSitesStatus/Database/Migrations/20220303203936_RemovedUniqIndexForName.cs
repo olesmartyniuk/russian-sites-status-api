735b033a15a8663daf81b504b78de513de147091
Oleksandr Martyniuk <alexander.martyniuk@gmail.com>
2022-03-03 15:45:00 -0500

    Removed uniq index from Site.Name

---
+++ src/RussianSitesStatus/Database/Migrations/20220303203936_RemovedUniqIndexForName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RussianSitesStatus.Database.Migrations
+{
+    public partial class RemovedUniqIndexForName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Sites_Name",
+                table: "Sites");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Sites_Name",
+                table: "Sites",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
