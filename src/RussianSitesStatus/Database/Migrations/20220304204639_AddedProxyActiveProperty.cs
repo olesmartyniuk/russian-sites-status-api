ac076860ee1d56d0ca376d816c0e38392a6f133e
Oleksandr Martyniuk <alexander.martyniuk@gmail.com>
2022-03-04 15:57:58 -0500

    Added ProxyIsActive property

---
+++ src/RussianSitesStatus/Database/Migrations/20220304204639_AddedProxyActiveProperty.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RussianSitesStatus.Database.Migrations
+{
+    public partial class AddedProxyActiveProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ProxyIsActive",
+                table: "Regions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProxyIsActive",
+                table: "Regions");
+        }
+    }
+}
