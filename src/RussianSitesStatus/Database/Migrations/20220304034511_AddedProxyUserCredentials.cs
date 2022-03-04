29c0bd443467942336602a00f1612863c818f306
Oleksandr Martyniuk <alexander.martyniuk@gmail.com>
2022-03-03 22:50:24 -0500

    Added proxy user credentials to Region model

---
+++ src/RussianSitesStatus/Database/Migrations/20220304034511_AddedProxyUserCredentials.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RussianSitesStatus.Database.Migrations
+{
+    public partial class AddedProxyUserCredentials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Regions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProxyPassword",
+                table: "Regions",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProxyUser",
+                table: "Regions",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Regions");
+
+            migrationBuilder.DropColumn(
+                name: "ProxyPassword",
+                table: "Regions");
+
+            migrationBuilder.DropColumn(
+                name: "ProxyUser",
+                table: "Regions");
+        }
+    }
+}
--- src/RussianSitesStatus/Database/Models/Region.cs
+++ src/RussianSitesStatus/Database/Models/Region.cs
@@ -6,7 +6,10 @@ namespace RussianSitesStatus.Database.Models;
 public class Region : Entity
 {
     public string Name { get; set; }
+    public string Code { get; set; }
     public string ProxyUrl { get; set; } //contains port
+    public string ProxyUser { get; set; }
+    public string ProxyPassword { get; set; }
 }
 
 public class RegionConfiguration : IEntityTypeConfiguration<Region>
