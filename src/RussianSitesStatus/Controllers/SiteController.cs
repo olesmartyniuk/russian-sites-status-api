732a772b6fd03b565d39bd2f62fbad8b2e7f20ea
Oleksandr Martyniuk <oles.martyniuk@gmail.com>
2022-03-07 14:45:37 -0500

    Added endpoint for check individual site status

--- src/RussianSitesStatus/Controllers/SiteController.cs
+++ src/RussianSitesStatus/Controllers/SiteController.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RussianSitesStatus.Auth;
 using RussianSitesStatus.Database.Models;
 using RussianSitesStatus.Extensions;
 using RussianSitesStatus.Models;
+using RussianSitesStatus.Models.Constants;
 using RussianSitesStatus.Services;
+using RussianSitesStatus.Services.Contracts;
 using System.Web;
 
 namespace RussianSitesStatus.Controllers;
@@ -13,12 +17,14 @@ public class SiteController : ControllerBase
     private readonly InMemoryStorage<SiteVM> _liteStatusStorage;
     private readonly InMemoryStorage<SiteDetailsVM> _fullStatusStorage;
     private readonly DatabaseStorage _databaseStorage;
+    private readonly ICheckSiteService _checkSiteService;
 
-    public SiteController(InMemoryStorage<SiteVM> liteStatusStorage, InMemoryStorage<SiteDetailsVM> fullStatusStorage, DatabaseStorage databaseStorage)
+    public SiteController(InMemoryStorage<SiteVM> liteStatusStorage, InMemoryStorage<SiteDetailsVM> fullStatusStorage, DatabaseStorage databaseStorage, ICheckSiteService checkSiteService)
     {
         _liteStatusStorage = liteStatusStorage;
         _fullStatusStorage = fullStatusStorage;
         _databaseStorage = databaseStorage;
+        _checkSiteService = checkSiteService;
     }
 
     /// <summary>
@@ -112,7 +118,7 @@ public class SiteController : ControllerBase
     /// <response code="409">Conflict</response> 
     /// <response code="500">Internal server error</response> 
     [HttpPost("api/sites/{siteUrl}")]
-   // [Authorize(AuthenticationSchemes = Scheme.ApiKeyAuthScheme)]
+    [Authorize(AuthenticationSchemes = Scheme.ApiKeyAuthScheme)]
     public async Task<ActionResult> Add([FromRoute()]string siteUrl)
     {
         siteUrl = HttpUtility
@@ -156,7 +162,7 @@ public class SiteController : ControllerBase
     /// <response code="404">Not found</response> 
     /// <response code="500">Internal server error</response> 
     [HttpDelete("api/sites/{siteId}")]
-    // [Authorize(AuthenticationSchemes = Scheme.ApiKeyAuthScheme)]
+    [Authorize(AuthenticationSchemes = Scheme.ApiKeyAuthScheme)]
     public async Task<ActionResult> Delete(long siteId)
     {
         var originalSite = await _databaseStorage.GetSite(siteId);
@@ -168,5 +174,71 @@ public class SiteController : ControllerBase
         await _databaseStorage.DeleteSite(originalSite.Id);
 
         return NoContent();
+    }
+
+    /// <summary>
+    /// Check site status by url. This call may take some time. Up to 30 sec.
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///    GET https://dev-russian-sites-status-api.herokuapp.com/api/sites/search?text=ya.ru
+    ///
+    /// </remarks>    
+    /// <param name="siteUrl">Site url with protocol</param>
+    /// <returns>Site status details</returns>    
+    /// <response code="200">Site status details</response>
+    /// <response code="400">Bad request</response> 
+    /// <response code="500">Internal server error</response> 
+    [HttpGet("api/sites/check/{siteUrl}")]
+    public async Task<ActionResult<SiteDetailsVM>> Check(string siteUrl)
+    {
+        if (string.IsNullOrEmpty(siteUrl) | siteUrl.Length < 3)
+        {
+            return BadRequest("The search text should contains more than 2 symbols");
+        }        
+
+        var regions = await _databaseStorage.GetRegions(true);        
+        var site = await _checkSiteService.CheckByUrl(siteUrl, regions);
+
+        var result = new SiteDetailsVM
+        {
+            Id = site.Id,
+            Name = site.Name,
+            Status = GetSiteStatus(site.Checks.Max(c => c.Status)),
+            Uptime = 0,
+            Servers = GetServers(site.Checks),
+            LastTestedAt = site.CheckedAt
+        };
+        
+        return Ok(result);
+    }
+
+    private List<ServerDto> GetServers(ICollection<Check> checks)
+    {
+        var servers = new List<ServerDto>();
+
+        foreach (var check in checks)
+            servers.Add(new ServerDto
+            {
+                Region = check.Region.Name,
+                RegionCode = check.Region.Code,
+                Status = GetSiteStatus(check.Status),
+                StatusCode = check.StatusCode
+            });
+
+        return servers;
+    }
+
+    private string GetSiteStatus(CheckStatus checkStatus)
+    {
+        var result = checkStatus switch
+        {
+            CheckStatus.Available => SiteStatus.Up,
+            CheckStatus.Unavailable => SiteStatus.Down,
+            CheckStatus.Unknown => SiteStatus.Unknown,
+            _ => throw new ArgumentOutOfRangeException(nameof(checkStatus), $"Not expected site status value: {checkStatus}"),
+        };
+        return result;
     }
 }
