af4f845e94e1faf2569f0c3e30bf7c84627baef5
Oleksandr Martyniuk <alexander.martyniuk@gmail.com>
2022-03-06 21:18:14 -0500

    Merged from master: Fixed incorrect response from incourse

--- src/RussianSitesStatus/Services/SiteSources/IncourseTradeSiteSource.cs
+++ src/RussianSitesStatus/Services/SiteSources/IncourseTradeSiteSource.cs
@@ -12,9 +12,35 @@ namespace RussianSitesStatus.Services
             var request = new RestRequest("sites.json", Method.Get);
             var response = await client.GetAsync(request);
 
-            var allSites = JsonSerializer.Deserialize<IEnumerable<IncourseSiteResponce>>(response.Content);          
+            var allSites = JsonSerializer.Deserialize<IEnumerable<IncourseSiteResponce>>(response.Content);
             
-            return allSites.Select(s => s.url);
+            var relevantSites = allSites
+                .Where(s => AsBoolean(s.atack) == true)
+                .ToList();
+
+            const int MaxNumberOfSites = 100;
+            var notRelevantSites = allSites
+                .Where(s => AsBoolean(s.atack) == false)
+                .Take(MaxNumberOfSites - relevantSites.Count);
+
+            relevantSites.AddRange(notRelevantSites);
+            
+            return relevantSites.Select(s => s.url);
+        }
+
+        private bool AsBoolean(object atack)
+        {
+            if (bool.TryParse(atack.ToString(), out var boolResult))
+            {
+                return boolResult;
+            }
+
+            if (int.TryParse(atack.ToString(), out var intResult))
+            {
+                return intResult == 1;
+            }
+
+            return false;
         }
 
         private class IncourseSiteResponce
@@ -24,7 +50,7 @@ namespace RussianSitesStatus.Services
             public int need_parse_url { get; set; }
             public string page { get; set; }
             public object page_time { get; set; }
-            public int atack { get; set; }
+            public object atack { get; set; }
         }
     }
 }
