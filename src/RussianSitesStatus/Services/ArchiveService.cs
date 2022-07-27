0709a978bf9e52055ac395ebd322cc090af16a43
Oleksandr Martyniuk <alexander.martyniuk@gmail.com>
2022-07-27 19:19:04 -0400

    Renamed some methods

--- src/RussianSitesStatus/Services/ArchiveService.cs
+++ src/RussianSitesStatus/Services/ArchiveService.cs
@@ -1,43 +1,32 @@
-using RussianSitesStatus.Database.Models;
-using RussianSitesStatus.Services.Contracts;
-using System.Collections.Concurrent;
-using System.Diagnostics;
-
 namespace RussianSitesStatus.Services;
 
 public class ArchiveService
 {
-    private readonly ILogger<MonitorSitesStatusService> _logger;
     private readonly IServiceScopeFactory _serviceScopeFactory;
 
-    public ArchiveService(
-        IServiceScopeFactory serviceScopeFactory,
-        ILogger<MonitorSitesStatusService> logger,
-        IConfiguration configuration)
+    public ArchiveService(IServiceScopeFactory serviceScopeFactory)
     {
         _serviceScopeFactory = serviceScopeFactory;
-        _logger = logger;
     }
 
     public async Task ArchiveOldData()
     {
-        using (var serviceScope = _serviceScopeFactory.CreateScope())
+        using var serviceScope = _serviceScopeFactory.CreateScope();
+        var databaseStorage = serviceScope.ServiceProvider
+            .GetRequiredService<DatabaseStorage>();
+
+        var minCheckedAt = await databaseStorage.GetMinCheckedAt();
+        var maxStatisticsDay = await databaseStorage.GetMaxStatistisDay();
+        if (!minCheckedAt.HasValue || !maxStatisticsDay.HasValue)
         {
-            var databaseStorage = serviceScope.ServiceProvider.GetRequiredService<DatabaseStorage>();
+            return;
+        }
 
-            var oldestChecksDateTime = await databaseStorage.GetOldestCheckSiteDate();
-            var lastStatisticsDateTime = await databaseStorage.GetNewestStatistisDate(); //TODOVK: check if statistis exist per site
-            if (!oldestChecksDateTime.HasValue || !lastStatisticsDateTime.HasValue)
-            {
-                return;
-            }
-
-            var oldestChecksDate = oldestChecksDateTime.Value.Date;
-            var lastStatisticsDate = lastStatisticsDateTime.Value.Date;
-            if (oldestChecksDate < lastStatisticsDate)
-            {
-                await databaseStorage.DeleteStatistis(lastStatisticsDate);
-            }
+        var minCheckedAtDate = minCheckedAt.Value.Date;
+        var maxStatisticsDayDate = maxStatisticsDay.Value.Date;
+        if (minCheckedAtDate < maxStatisticsDayDate)
+        {
+            await databaseStorage.DeleteChecksBefore(maxStatisticsDayDate);
         }
     }
 }
